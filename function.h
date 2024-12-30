a6dc0d3b5f6e9bd91c1bfa76c6ec37fefd6ace31
thang02122006 <tranvanthang02122006@gmail.com>
2024-12-30 12:57:28 +0700

    Update function.h

--- function.h
+++ function.h
@@ -2,12 +2,12 @@
 
 // nguyen mau ham
 void displayMenu();
-void printStore(Store store[], int length);  
-void addStore(Store stores[], int *length);  
-void updateStore(Store store[], int length);  
+void printstore(Store store[], int length);  
+void addstore(Store stores[], int *length);  
+void dateStore(Store store[], int length);  
 void deleteStore(Store store[], int *length); 
 void searchStoreByName(Store store[], int length);  
 void toLowerCase(char str[]);
-void sortStoresByPrice(Store stores[], int length, int ascending);  
-void saveStoresToFile(Store store[], int length);  
-void loadStoresFromFile(Store stores[], int *length);  
+void sorStoresByrice(Store stores[], int length, int ascending);  
+void saveStoreoFile(Store store[], int length);  
+void loadStoreFromFile(Store stores[], int *length);  
